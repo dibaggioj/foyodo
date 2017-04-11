fe1105be14c507e885ab3c44240ab290ac070503
John DiBaggio <jsantoku@gmail.com>
2017-04-11 04:22:47 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FoYoDo
+
+## Authors
+Nanu Ahluwalia
+John DiBaggio
+Kyle Etsler
+
+## Hardware
+- Raspberry Pi B+ (or newer)
+- Raspberry Camera
+- Dymo M10 USB scale
+- HC-SR04 Ultrasonic Sensor
---
+++ read-scale.py
@@ -0,0 +1,59 @@
+import usb.core
+import usb.util
+import sys
+
+VENDOR_ID = 0x0922  # DYMO
+PRODUCT_ID = 0x8003 # M10
+
+# find the USB device
+device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+
+if device is None:
+    sys.exit("Could not find scale")
+
+if device.is_kernel_driver_active(0):
+    try:
+        device.detach_kernel_driver(0)
+        print "kernel driver detached"
+    except usb.core.USBError as e:
+        sys.exit("Could not detach kernel driver: %s" % str(e))
+else:
+    print "no kernel driver attached"
+
+# try:
+#     usb.util.claim_interface(device, 0)
+#     print "claimed device"
+# except:
+#     sys.exit("Could not claim the device: %s" % str(e))
+
+try:
+    # use the first/default configuration
+    device.set_configuration()
+    device.reset()
+
+    try:
+        usb.util.claim_interface(device, 0)
+        print "claimed device"
+    except:
+        sys.exit("Could not claim the device: %s" % str(e))
+
+    # first endpoint
+    endpoint = device[0][(0,0)][0]
+
+    # read a data packet
+    attempts = 10
+    data = None
+    while data is None and attempts > 0:
+        try:
+            data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+        except usb.core.USBError as e:
+            data = None
+            if e.args == ("Operation timed out",):
+                attempts -= 1
+                continue
+    print data
+
+except usb.core.USBError as e:
+    sys.exit("Could not set configuration: %s" % str(e))
+
+
