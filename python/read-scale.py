6a154e8c2d288b7b2bd1d6f7fbd57949313d0c34
John DiBaggio <john.dibaggio@vml.com>
2017-04-10 22:44:53 -0600

    read scale ever second

--- python/read-scale.py
+++ python/read-scale.py
@@ -1,9 +1,30 @@
+import os
 import usb.core
 import usb.util
 import sys
+import time
 
 VENDOR_ID = 0x0922  # DYMO
 PRODUCT_ID = 0x8003 # M10
+
+def listen_for_weight():
+    print "listening for weight..."
+
+    while True:
+        time.sleep(1)
+
+        # read a data packet
+        attempts = 10
+        data = None
+        while data is None and attempts > 0:
+            try:
+                data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+            except usb.core.USBError as e:
+                data = None
+                if e.args == ("Operation timed out",):
+                    attempts -= 1
+                    continue
+        print data
 
 # find the USB device
 device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
@@ -20,12 +41,6 @@ if device.is_kernel_driver_active(0):
 else:
     print "no kernel driver attached"
 
-# try:
-#     usb.util.claim_interface(device, 0)
-#     print "claimed device"
-# except:
-#     sys.exit("Could not claim the device: %s" % str(e))
-
 try:
     # use the first/default configuration
     device.set_configuration()
@@ -40,18 +55,7 @@ try:
     # first endpoint
     endpoint = device[0][(0,0)][0]
 
-    # read a data packet
-    attempts = 10
-    data = None
-    while data is None and attempts > 0:
-        try:
-            data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-        except usb.core.USBError as e:
-            data = None
-            if e.args == ("Operation timed out",):
-                attempts -= 1
-                continue
-    print data
+    listen_for_weight()
 
 except usb.core.USBError as e:
     sys.exit("Could not set configuration: %s" % str(e))
