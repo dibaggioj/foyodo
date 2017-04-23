c9dad373484f8480eaf618d6f59ed596861a2d67
johndibaggio <jsantoku@gmail.com>
2017-04-23 01:01:06 -0600

    add comments to main

--- foyodo/main.py
+++ foyodo/main.py
@@ -9,7 +9,13 @@ def main():
     try:
         scale = Scale()
         scale.start()
+
+        # call scale.lock_previous_weight when motion is detected to stop updating the stable weight value
+        # call scale.is_weight_reduced when motion is no longer detected to see if the stable weight has decreased
+        # call scale.lock_previous_weight when motion is no longer detected to start updating the stable weight value again
+
         signal.pause()
+
     except (KeyboardInterrupt, SystemExit):
         print "\nStopping FoYoDo program...\n"
         scale.stop()
