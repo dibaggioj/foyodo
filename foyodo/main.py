618392df3696eab8baa4720c1421576698b02b57
johndibaggio <jsantoku@gmail.com>
2017-04-23 18:35:29 -0600

    Fix indents

--- foyodo/main.py
+++ foyodo/main.py
@@ -76,18 +76,18 @@ def main():
                 time.sleep(10)
                 while True:
                     GPIO.output(TRIG, True)
-                        time.sleep(0.00001)
-                        GPIO.output(TRIG, False)
+                    time.sleep(0.00001)
+                    GPIO.output(TRIG, False)
 
-                        while GPIO.input(ECHO)==0:
-                                pulse_start = time.time()
+                    while GPIO.input(ECHO)==0:
+                        pulse_start = time.time()
 
-                        while GPIO.input(ECHO)==1:
-                                pulse_end = time.time()
+                    while GPIO.input(ECHO)==1:
+                        pulse_end = time.time()
 
-                        pulse_duration = pulse_end - pulse_start
-                        distance = pulse_duration * 17150
-                        distance = round(distance, 2)
+                    pulse_duration = pulse_end - pulse_start
+                    distance = pulse_duration * 17150
+                    distance = round(distance, 2)
                     # print "Recording, distance away: ",distance ,"cm"
 
                     if distance >= 30:
