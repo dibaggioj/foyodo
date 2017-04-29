43a2d3b31cd55ddba6c59dee3070c236efc0261b
johndibaggio <jsantoku@gmail.com>
2017-04-29 17:17:36 -0600

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,9 +5,13 @@ if [[ $answer = Y ]] || [[ $answer = y ]] ; then
     # http://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
 	sudo cp checkwifi.sh /usr/local/bin/checkwifi.sh
     sudo chmod 775 /usr/local/bin/checkwifi.sh
-    printf "Copy the following line and paste it at the bottom of the /tmp/crontab.rDmnwm/crontab file in the Nano editor:"
-    printf "*/5 * * * * /usr/bin/sudo -H /usr/local/bin/checkwifi.sh >> /dev/null 2>&1"
-    crontab -e
+    read -p "Copy the following line and paste it at the bottom of the /tmp/crontab.rDmnwm/crontab file in the Nano editor. Press ENTER once you've copied it:`echo $'\n\n*/5 * * * * /usr/bin/sudo -H /usr/local/bin/checkwifi.sh >> /dev/null 2>&1\n\n'``echo $'\n> '`" copy
+    if [[ $copy = "" ]]; then
+        crontab -e
+    else
+        echo "You pressed '$key'"
+    fi
+
 fi
 
 read -p "Install project Python requirements?`echo $'\n[Y/n]?'``echo $'\n> '`" answer
