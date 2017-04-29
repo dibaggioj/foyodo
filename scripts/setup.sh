7c002ffdd30740ecad0528ab36fa3b782cf395d8
johndibaggio <jsantoku@gmail.com>
2017-04-28 21:28:14 -0600

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,16 @@
-sudo pip install -r ../requirements.txt
+#!/bin/bash
+
+read -p "Setup WiFi network auto reconnect?`echo $'\n[Y/n]?'``echo $'\n> '`" answer
+if [[ $answer = Y ]] || [[ $answer = y ]] ; then
+    # http://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
+	sudo cp checkwifi.sh /usr/local/bin/checkwifi.sh
+    sudo chmod 775 /usr/local/bin/checkwifi.sh
+    printf "Copy the following line and paste it at the bottom of the /tmp/crontab.rDmnwm/crontab file in the Nano editor:"
+    printf "*/5 * * * * /usr/bin/sudo -H /usr/local/bin/checkwifi.sh >> /dev/null 2>&1"
+    crontab -e
+fi
+
+read -p "Install project Python requirements?`echo $'\n[Y/n]?'``echo $'\n> '`" answer
+if [[ $answer = Y ]] || [[ $answer = y ]] ; then
+    sudo pip install -r ../requirements.txt
+fi
