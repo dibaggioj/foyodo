d48abfa1676ed17f6a677dffb83d86d787c80f5e
johndibaggio <jsantoku@gmail.com>
2017-05-02 10:18:06 -0600

    Take a picture from scale code when the weight reduces

--- foyodo/lib/fyd_camera.py
+++ foyodo/lib/fyd_camera.py
@@ -1,14 +1,13 @@
-import picamera.PiCamera as PiCamera
-import picamera.PiCameraRuntimeError as PiCameraRuntimeError
+import picamera
 
 
-class FydCamera(PiCamera):
+class FydCamera(picamera.PiCamera):
     def is_camera_recording(self):
         if self.camera is None:
             return True  # don't try to take a picture if the camera is null
         try:
             self._check_recording_stopped()
             return False
-        except PiCameraRuntimeError as e:
+        except picamera.PiCameraRuntimeError as e:
             print("Camera is currently recording: %s" % str(e))
             return True
