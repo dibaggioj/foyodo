3682f4fdb7d60cd405366e419c7a3f7a64db55bd
johndibaggio <jsantoku@gmail.com>
2017-05-02 21:05:55 -0600

    Swap out ultrasonic sensor code for PIR sensor code

--- foyodo/lib/capture.py
+++ foyodo/lib/capture.py
@@ -34,13 +34,8 @@ class Capture(threading.Thread):
 
     PIN_INPUT_PIR = 18
 
-    TIME_SEC_PIR_CALIBRATION = 15   # Time given to PIR sensor to calibrate (10-60 secs according to the data sheet)
-    TIME_MS_PAUSE = 5000
-
-    DISTANCE_CM_CLOSE = 10
-    DISTANCE_CM_FAR = 30
-
-    DISTANCE_READING_COUNT = 5
+    TIME_SEC_PIR_CALIBRATION = 10   # Time given to PIR sensor to calibrate (10-60 secs according to the data sheet)
+    TIME_SEC_PAUSE = 10             # Stop recording video if no motion is sensed for 10 seconds
 
     recordflag = False
     idleflag = True
@@ -101,106 +96,50 @@ class Capture(threading.Thread):
         print "## Starting scale..."
         self.scale.start()
         while not self.stopped():
-            if GPIO.input(self.PIN_INPUT_PIR) == GPIO.HIGH:
-                print("HIGH")
+            while self.idleflag is True:
+                if GPIO.input(self.PIN_INPUT_PIR) == GPIO.HIGH:
+                    print("HIGH")
 
-                if self.lock_low:
-                    print("Lock is low")
-                    self.lock_low = False
-                    print("---")
-                    print("Motion detected at %s millisec" % time.time())
-                    # time.sleep(0.05);
+                    if self.lock_low:
+                        print("Lock is low")
+                        self.lock_low = False
+                        print("---")
+                        print("Motion detected at %s millisec" % time.time())
+                        self.idleflag = False
+                        self.recordflag = True
+                        # time.sleep(0.05);
 
-                take_low_time = True
+                    take_low_time = True
 
-            if GPIO.input(PIN_INPUT_PIR) == GPIO.LOW:
-                print("LOW")
-                if take_low_time:
-                    low_in = time.time()    ## save the time of the transition from high to LOW
-                    take_low_time = False   ## make sure this is only done at the start of a LOW phase
+            while self.recordflag is True:
+                print "Start Recording"
+                ts = time.time()
+                vid_name = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+                self.camera.capture(os.getcwd() + '/picture/'+vid_name+'.jpg')
+                self.camera.start_recording(os.getcwd() + '/video/'+vid_name+'.h264')
 
-                # if the sensor is low for more than the given pause,
-                # we assume that no more motion is going to happen
-                if not lock_low and (time.time() - low_in) > TIME_MS_PAUSE:
-                    # makes sure this block of code is only executed again after
-                    # a new motion sequence has been detected
-                    lock_low = True
-                    print("Motion ended at %s millisec" % (time.time() - TIME_MS_PAUSE))
-                    # time.sleep(0.05);
-            #
-            #
-            # distance_readings = []
-            # while self.idleflag is True:
-            #     try:
-            #         GPIO.output(self.TRIG, True)
-            #         time.sleep(0.00001)
-            #         GPIO.output(self.TRIG, False)
-            #
-            #         while GPIO.input(self.ECHO) == 0:
-            #             pulse_start = time.time()
-            #
-            #         while GPIO.input(self.ECHO) == 1:
-            #             pulse_end = time.time()
-            #
-            #         pulse_duration = pulse_end - pulse_start
-            #         distance = pulse_duration * 17150
-            #         distance = round(distance, 2)
-            #
-            #         distance_readings.append(distance)
-            #
-            #         if len(distance_readings) >= self.DISTANCE_READING_COUNT:
-            #             distance_stable = numpy.median(distance_readings)
-            #             distance_readings = []
-            #             print("Stable distance: %s cm" % distance_stable)
-            #             if distance_stable <= self.DISTANCE_CM_CLOSE:
-            #                 self.idleflag = False
-            #                 self.recordflag = True
-            #     except:
-            #         print "Exception occurred while recording distance"
-            # print "movement detected"
-            # self.scale.lock_previous_weight()
-            # print("Locking weight at: %s" % self.scale.weight_lock)
-            #
-            # while self.recordflag is True:
-            #     print "Start Recording"
-            #     ts = time.time()
-            #     vid_name = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-            #     self.camera.capture(os.getcwd() + '/picture/'+vid_name+'.jpg')
-            #     self.camera.start_recording(os.getcwd() + '/video/'+vid_name+'.h264')
-            #     time.sleep(10)
-            #     while True:
-            #         try:
-            #             GPIO.output(self.TRIG, True)
-            #             time.sleep(0.00001)
-            #             GPIO.output(self.TRIG, False)
-            #
-            #             while GPIO.input(self.ECHO) == 0:
-            #                 pulse_start = time.time()
-            #
-            #             while GPIO.input(self.ECHO) == 1:
-            #                 pulse_end = time.time()
-            #
-            #             pulse_duration = pulse_end - pulse_start
-            #             distance = pulse_duration * 17150
-            #             distance = round(distance, 2)
-            #
-            #             distance_readings.append(distance)
-            #
-            #             if len(distance_readings) >= self.DISTANCE_READING_COUNT:
-            #                 distance_stable = numpy.median(distance_readings)
-            #                 distance_readings = []
-            #                 print("Recording, stable distance away: %s cm" % distance_stable)
-            #                 if distance_stable >= self.DISTANCE_CM_FAR:
-            #                     break
-            #
-            #         except:
-            #             print "Exception occurred while recording distance"
-            #
-            #     print "stop recording"
-            #     self.camera.stop_recording()
-            #     self.recordflag = False
+                while True:
+                    if GPIO.input(self.PIN_INPUT_PIR) == GPIO.LOW:
+                        print("LOW")
+                        if take_low_time:
+                            self.low_in = time.time()   ## save the time of the transition from high to LOW
+                            self.take_low_time = False  ## make sure this is only done at the start of a LOW phase
 
-            time.sleep(5)
+                        # if the sensor is low for more than the given pause,
+                        # we assume that no more motion is going to happen
+                        if not self.lock_low and (time.time() - self.low_in) > self.TIME_SEC_PAUSE:
+                            # makes sure this block of code is only executed again
+                            # after a new motion sequence has been detected
+                            self.lock_low = True
+                            print("Motion ended at %s millisec" % (time.time() - self.TIME_SEC_PAUSE))
+                            break
+                            # time.sleep(0.05);
+
+                print "Stop recording"
+                self.camera.stop_recording()
+                self.recordflag = False
+
+            time.sleep(5)   # Wait until video finishes encoding
             print("Current weight is: %s" % self.scale.weight_current)
             print("Done recording video. Is weight reduced: %s" % self.scale.is_weight_reduced())
 
