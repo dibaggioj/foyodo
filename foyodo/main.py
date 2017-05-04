b635dd2c0044a8746a6dddaa775042c96f79b73e
johndibaggio <jsantoku@gmail.com>
2017-05-04 16:43:40 -0600

    Comment-out execution for now

--- foyodo/main.py
+++ foyodo/main.py
@@ -1,26 +1,55 @@
 import os
+import RPi.GPIO as GPIO
 import signal
 import sys
+import time
 
 from lib.capture import Capture
 
 
 def main():
-    print "Starting FoYoDo program..."
+    PIN_INPUT_SWITCH = 23
 
     # Set current directory to directory containing main.py. Can get this directory elsewhere with `os.getcwd()`
     os.chdir(os.path.realpath(__file__+"/.."))
 
-    capture = Capture()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN_INPUT_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Switch set as input w/ pull-up
+
+    running = False
+    capture = None
+
+    print("Flip switch to start FoYoDo.")
 
     try:
-        capture.start()
-        signal.pause()  # sleep until KeyboardInterrupt or SystemExit signal is received
+        while not running:
+            if GPIO.input(PIN_INPUT_SWITCH) != GPIO.HIGH:
+                print "Starting FoYoDo program..."
+
+                running = True
+
+                # TODO: uncomment
+                # capture = Capture()
+                # capture.start()
+                signal.pause()  # sleep until KeyboardInterrupt or SystemExit signal is received
+
+            time.sleep(1)
+
+        while running:
+            print("Flip switch to stop FoYoDo.")
+            if GPIO.input(PIN_INPUT_SWITCH) == GPIO.HIGH:
+                raise SystemExit("Flipped switch to stop FoYoDo.")
+
+            time.sleep(1)
 
     except (KeyboardInterrupt, SystemExit):
         print "Stopping FoYoDo program..."
-        capture.stop()
-        capture.join()
+
+        if capture is not None:
+            capture.stop()
+            capture.join()
+
         sys.exit()
+
 
 main()
